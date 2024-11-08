896b5e1a184a0b8832b4cbadbf01f4b1a68ca0bf
samudra1024 <samudrahosmata@gmail.com>
2024-11-08 10:44:50 +0530

    8th nov 2024

---
+++ ddl_employee.c
@@ -0,0 +1,70 @@
+/*
+WAP to implement DDL with following data feilds for the employee record 
+    1.SSN()
+    2.name
+    3.department 
+    4.designation
+    5.salary
+*/
+#include<stdio.h>
+#include<stdlib.h>
+typedef struct EMPLIST 
+{
+    char SSN[10];
+    char name[10];
+    char dept[10];
+    char desig[10];
+    float sal;
+    struct EMPLIST *rlink;
+    struct EMPLIST *llink;
+}enode;
+typedef struct EMPHEAD
+{
+    int data;
+    enode *rlink;
+    enode *llink;
+}ehead;
+ehead *insert_front(ehead *h);
+ehead *insert_rear(ehead *h);
+ehead *delete_front(ehead *h);
+ehead *delete_rear(ehead *h);
+ehead *del_specific(ehead *h);
+void display(ehead *h);
+int main()
+{
+    ehead *head=(ehead*)malloc(sizeof(ehead));
+    head->data=0;
+    head->llink=head->rlink=NULL;
+    int ch;
+    enode *first;
+    char n[10],s[10],d[10],dg[10];
+    float sar;
+    while(1)
+    {
+        printf("1.insert front\n");
+        printf("2.insert rear\n");
+        printf("3.delete front\n");
+        printf("4.delete rear\n");
+        printf("5.delete specific element\n");
+        printf("6.traverse\n");
+        printf("7.exit\n");
+        printf("Enter your choice : ");
+        scnaf("%d",&ch);
+        switch (ch) 
+        {
+            case 1: head=insert_front(head);
+                    break;
+            case 2: head=insert_rear(head);
+                    break;
+            case 3: head=delete_front(head);
+                    break;
+            case 4: head=delete_rear(head);
+                    break;
+            case 5: head=del_specific(head);
+                    break;
+            case 6: display(head);
+                    break;
+            case 7:exit(0);
+        }
+    }
+}
